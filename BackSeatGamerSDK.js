89786f370a0ffe6e845be029300ae47303c6fa5a
Christopher <cpsuperstoreinc@gmail.com>
2022-05-23 00:17:01 -0400

    Initial commit

---
+++ BackSeatGamerSDK.js
@@ -0,0 +1,48 @@
+var REWARDS = {}
+
+function postMessage(message){
+    try {
+        park.postMessage({
+            type: "award",
+            text: message
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function main() {
+    postMessage("BackSeatGamer Initialized...");
+
+    var server = network.createListener();
+    server.on('connection', function (conn) {
+        conn.on('data', function(data) {
+            conn.write("done");
+
+            data = JSON.parse(data);
+
+            var command = data["command"];
+            var reward = data["name"];
+            var user = data["guest"];
+
+            if(REWARDS.hasOwnProperty(command)){
+                postMessage(user + " has redeemed the reward " + reward);
+                REWARDS[command](user, reward, command);
+
+            } else {
+                postMessage("Unknown Command: " + command);
+            }
+        });
+    });
+    server.listen(8080);
+}
+
+registerPlugin({
+    name: 'BackSeatGamer OpenRCT2 SDK',
+    version: '1.0',
+    authors: [],
+    type: 'remote',
+    licence: 'MIT',
+    targetApiVersion: 34,
+    main: main
+});
